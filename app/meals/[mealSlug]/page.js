3aa35383041eeaaa1bba585ea58e00273df2299e
Ujjwal <ujjwalgupta220505@gmail.com>
2026-03-14 11:10:44 +0530

    Completed the browse meals section

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,30 @@
+import classes from './page.module.css';
+import {getMeal} from '@/lib/meals';
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+export default async function MealDetailsPage({params}) {
+  const {mealSlug}=await params;
+  const meal=getMeal(mealSlug);
+  if(!meal){
+    notFound();
+  }
+  meal.instructions=meal.instructions.replace(/\n/g,`<br/>`);
+  return (
+    <>
+    <header className={classes.header}>
+      <div className={classes.image}>
+        <Image src={meal.image} fill/>
+      </div>
+      <div className={classes.headerText}>
+        <h1>{meal.title}</h1>
+        <p className={classes.creator}>by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a></p>
+        <p className={classes.summary}>{meal.summary}</p>
+      </div>
+    </header>
+    <main className={classes.instructions} dangerouslySetInnerHTML={{
+      __html:meal.instructions,
+    }}>
+    </main>
+    </>
+  );
+}
